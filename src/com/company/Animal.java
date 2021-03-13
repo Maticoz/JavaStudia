652aac2daad650daff1f816b028ecadc082d01e0
Matico <marekderela@gmail.com>
2021-03-13 13:44:46 +0100

    Task1 - Pets feeding process

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -2,13 +2,60 @@ package com.company;
 import java.io.File;
 
 public class Animal {
+    public static final String DOG  =    "DOG";
+    public static final String LION =   "LION";
+    public static final String CAT  =    "CAT";
+
     String name;
-    public Double weight;
-    public String species;
+    private Double weight;
+    public final String species;
     File pic;
 
-    public Animal()
+    public Animal(String species)
     {
+        this.species = species;
+        this.setWeightSpecies();
+    }
+
+    public void feed() {
+        this.weight += 1.0;
+        System.out.println("Wrrr...");
+    }
+    public void takeForAWalk() {
+        this.weight -= 1.0;
+        if(this.weight <= 0)
+        {
+            System.out.println("Zwierze umarło z głodu.");
+            return;
+        }
+        System.out.println("Grrr....");
+
+    }
+
+    protected void setWeightSpecies()
+    {
+        switch (this.species)
+        {
+            case DOG:
+            {
+                this.weight = 8.0;
+                break;
+            }
+            case LION:
+            {
+                this.weight = 80.0;
+                break;
+            }
+            case CAT:
+            {
+                this.weight = 2.0;
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,37 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Animal dog = new Animal();
-        dog.species = "rasa";
-        dog.name = "Szarik";
-        dog.weight = 6.0;
-        System.out.println("Pies: ");
+        Animal dog = new Animal(Animal.CAT);
+        dog.name = "Mruczek";
+        System.out.println("Kot: ");
         System.out.println(dog.species);
         System.out.println(dog.name);
-        System.out.println(dog.weight);
 
-        System.out.println("Człowiek: ");
+        dog.feed();
 
-        Human human = new Human();
-        human.age = 20;
-        human.firstName = "Marek";
-        human.lastName = "Kowalski";
-        human.gender = "Mężczyzna";
-
-        System.out.println(human.firstName);
-        System.out.println(human.lastName);
-        System.out.println(human.gender);
-        System.out.println(human.age);
-
-        Animal cat = new Animal();
-        cat.species = "Kotek";
-        cat.name = "Cat";
-        cat.weight = 2.0;
-
-        System.out.println("Kot: ");
-        System.out.println(cat.species);
-        System.out.println(cat.name);
-        System.out.println(cat.weight);
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
 
     }
 }
